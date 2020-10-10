9ee6a514bca6eace63853c60cc1de629f42f9512
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-10-11 03:59:53 +0530

    [Prajval] Asks user to choose the character with which he wants to play the game

--- Program.cs
+++ Program.cs
@@ -6,9 +6,11 @@ namespace TicTacToeFinal
     {
         static void Main(string[] args)
         {
-            TicTacToeGame call = new TicTacToeGame();
+            TicTacToeGame call = new TicTacToeGame(); // this object call is used to call the functions of class TicTacToeGmae
             Console.WriteLine("Welcome to the Tic Tac Toe Program");
             char[] currentBoard = call.CreateBoard();
+            char userInput = call.ZeroOrCross();
+            char computerInput = call.ComputerInput(userInput);
         }
     }
 }
--- TicTacToeGame.cs
+++ TicTacToeGame.cs
@@ -6,6 +6,10 @@ namespace TicTacToeFinal
 {
     class TicTacToeGame
     {
+        /// <summary>
+        /// Creates the board of size 10 with all the elements having null value.
+        /// </summary>
+        /// <returns></returns>
         public char[] CreateBoard()
         {
             char[] board = new char[10];
@@ -14,6 +18,40 @@ namespace TicTacToeFinal
                 board[i] = ' ';
             }
             return board;
+        }
+        /// <summary>
+        /// Asks the user to choose the character he wants to play the game with. choices being 'O' or 'X'
+        /// </summary>
+        /// <returns></returns>
+        public char ZeroOrCross()
+        {
+            char input;
+            for (; ; )
+            {
+                Console.WriteLine("Enter the chracter, 'O' or 'X', you'd want to play with");
+                input = Convert.ToChar(Console.ReadLine().ToUpper());
+                if (input == 'O' || input == 'X')
+                    break;
+                Console.WriteLine("Invalid Input");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            return input;
+        }
+        /// <summary>
+        /// Assigns the computer the character with which it is to play the game. If user chooses 'O' then computer plays 
+        /// with 'X' and vice-versa.
+        /// </summary>
+        /// <param name="userInput">The user input.</param>
+        /// <returns></returns>
+        public char ComputerInput(char userInput)
+        {
+            char computerInput;
+            if (userInput == 'X')
+                computerInput = 'O';
+            else
+                computerInput = 'X';
+            return computerInput;
         }
     }
 }
